b2730e216d7985b573c1909a4f522e1c4dc9aa98
Mattthew Lusty <mattlusty123@gmail.com>
2022-01-05 21:49:51 +0000

    cont: add to descriptions to Extra[curricula]

--- src/components/Extra.jsx
+++ src/components/Extra.jsx
@@ -2,28 +2,52 @@ import "../styles/css/Extra.css";
 
 function Extra(props) {
   return (
-    <div className="Extra card">
-      <h2 className="headerStrip">Extracurricula Activities</h2>
-      <div className="content">
-        <div className="job">
-          <div className="title">Volunteer work for Sea Cadets</div>
-          <div className="content">(Led lessons and supervised cadets trips)</div>
-        </div>
-        <div className="job">
-          <div className="title">Gold Duke of Edinburgh Award</div>
-          <div className="content">(Expedition across Royal National Park, Sydney)</div>
-        </div>
-        <div className="job">
-          <div className="title">Three Peaks Challenge UK</div>
-          <div className="content">(Climbed UK’s three highest peaks in three days)</div>
-        </div>
-        <div className="job">
-          <div className="title">Travelling Australasia and Asia</div>
-        </div>
-        <div className="job">
-          <div className="title">Wild camping across the Outer Hebrides</div>
+    <div>
+      <div className="Extra card">
+        <h2 className="headerStrip">Extracurricula Experiences</h2>
+        <div className="content">
+          <div className="job">
+            <div className="title">Volunteer work for Sea Cadets</div>
+            <div className="content">Led lessons and supervised cadets trips</div>
+          </div>
+          <div className="job">
+            <div className="title">Gold Duke of Edinburgh Award</div>
+            <div className="content">Expedition across Royal National Park, Sydney</div>
+          </div>
+          <div className="job">
+            <div className="title">Three Peaks Challenge UK</div>
+            <div className="content">Climbed UK’s three highest peaks</div>
+          </div>
+          <div className="job">
+            <div className="title">Travelling Australasia and Asia</div>
+            <div className="content">Horse riding, scuba diving, friends and memories</div>
+          </div>
+          <div className="job">
+            <div className="title">10 day Hike across the Outer Hebrides</div>
+            <div className="content">Torrential weather with only a tarp for a tent.</div>
+          </div>
         </div>
       </div>
+      {/* <div className="Extra card">
+        <h2 className="headerStrip">Extracurricula Experiences</h2>
+        <div className="content">
+          <div className="job">
+            <div className="title">Squash and Tennis</div>
+          </div>
+          <div className="job">
+            <div className="title">Kayaking, Canoeing and Rowing</div>
+          </div>
+          <div className="job">
+            <div className="title">Hiking, camping and exploring</div>
+          </div>
+          <div className="job">
+            <div className="title">Skateboarding and Snowboardin</div>
+          </div>
+          <div className="job">
+            <div className="title">Cooking and creating new recipes</div>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
